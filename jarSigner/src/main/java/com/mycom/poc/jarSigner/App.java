672c9e61f91d82bd18c77f382218bbfcd2cb3332
makarandbagade <42614761+makarandbagade@users.noreply.github.com>
2018-08-22 21:07:20 +0530

    adding new project

---
+++ jarSigner/src/main/java/com/mycom/poc/jarSigner/App.java
@@ -0,0 +1,14 @@
+package com.mycom.poc.jarSigner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World 1" );
+        System.out.println( "Hello World 2" );
+    }
+}
